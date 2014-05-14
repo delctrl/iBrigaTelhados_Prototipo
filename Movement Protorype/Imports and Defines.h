5e2d3f435c0b85f45382542bcf582f8db90a0775
LeoGigliotti <leogig.lol@live.com>
2014-05-14 15:39:50 -0300

    adicionada a padronização de código

--- Movement Protorype/Imports and Defines.h
+++ Movement Protorype/Imports and Defines.h
@@ -21,4 +21,32 @@
 #define MAP_H 5
 #define MAP_W 5
 
+
+/**
+ Padronização do Código:
+ 
+ O código será escrito inteiramente em inglês, com comentários em português.
+ Nomes de classes seguirão o formato: BPTName.
+ Toda classe deve ser importada pelo arquivo Imports and Defines, o qual deve ser importado por todas as classes.
+ Nomes de variáveis começarão com a abreviação do nome da classe. Ex.: Character * charName; Tile * tileName; UIButton * btnName.
+ Tentar utilizar ao máximo arrays, ao invés de criar vários objetos como propriedades.
+ 
+ Comentários seguirão o seguinte padrão:
+ 
+ @b---------    Branch mãe.
+ @version---    Versão do código, com breve comentário das mudanças.
+ @date------    Data da última - grande - mudança.
+ @author----    Autor da última - grande - mudança.
+ @test------    Referência do que deve ser incluido nos testes automatizados.
+ @details---    Explicações detalhadas de um método ou parte do código.
+ @note------    Notas breves, sobre uma parte do código.
+ @bug-------    Bug que ainda falta ser trabalhado.
+ @skip------    Parte do código não utilizada, que pode ser útil futuramente, ou parte do código comentada, por qualquer outro motivo.
+ 
+ **/
+
+
+
+
+
 #endif
